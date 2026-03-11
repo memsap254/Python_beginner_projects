ed91b7cb3b0ce074561f2549a942655f5c60673b
memsap254 <leah.gathungu24@gmail.com>
2026-03-11 18:13:33 +0300

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Division:", division)
